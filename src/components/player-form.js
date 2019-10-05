085e5baa379f2950eb6d88f6fa29032cf495c160
Liz <40321604+lizkavalski@users.noreply.github.com>
2019-10-05 14:58:57 -0700

    Basiclly done

--- src/components/player-form.js
+++ src/components/player-form.js
@@ -1,40 +1,38 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
 let PlayerForm = props => {
+	const data = props.initialValues || {};
 
-  const data = props.initialValues || {};
+	let [form, setForm] = useState(data);
 
-  let [form,setForm] = useState(data);
+	const handleChange = e => {
+		let { name, value } = e.target;
+		setForm({ ...form, [name]: value });
+	};
 
-  const handleChange = (e) => {
-    let {name,value} = e.target;
-    setForm({...form, [name]:value});
-  };
+	const handleSubmit = e => {
+		e.preventDefault();
+		props.onSubmit(form);
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    props.onSubmit(form);
-  };
+	useEffect(() => {
+		setForm(data);
+	}, []);
 
-  useEffect( () => {
-    setForm(data);
-  }, [data]);
-
-
-  return (
-    <form onChange={handleChange} onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input defaultValue={data.name} name="name" />
-      </div>
-      <div>
-        <label htmlFor="team">Team</label>
-        <input defaultValue={data.team} name="team" />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  );
+	return (
+		<form onChange={handleChange} onSubmit={handleSubmit}>
+			<div>
+				<label htmlFor="name">Name</label>
+				<input defaultValue={data.name} name="name" />
+			</div>
+			<div>
+				<label htmlFor="team">Team</label>
+				<input defaultValue={data.team} name="team" />
+			</div>
+			<button type="submit">Submit</button>
+		</form>
+	);
 };
 
 export default PlayerForm;
